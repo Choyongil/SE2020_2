02a0139153009510bdeaea3465748fe47f05c70e
Choyongil <cyi1207@naver.com>
2020-03-19 22:56:06 +0900

    add function to program 1

--- exercise.py
+++ exercise.py
@@ -1,5 +1,9 @@
-print("push number!")
+print("press number(1 or 2)! If you don't want anything, press 0")
 n = int(input())
 
 if n == 1:
     print("Choyongil")
+elif n == 2:
+    print("Hi. I'm Choyongil. I like listening to music. My hobby is basketball.")
+else:
+    print("Bye")
